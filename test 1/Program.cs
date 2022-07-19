211246b2ac110c29f60a5cdaeb24c2b2b4db76f8
Lap <tranlap74@gmail.com>
2022-07-19 21:27:54 +0700

    Tinh Tong

---
+++ test 1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace test_1
+{
+    internal class Program
+    {
+        static int Tong(int a,int b)
+        {
+            return a + b;  
+        }
+        static void Main(string[] args)
+        {
+            int x = 2;
+            int y = 3;
+            Console.WriteLine(Tong(x,y));
+
+        }
+    }
+}
